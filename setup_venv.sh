525d50b99efd2aae1ae2fbda8d20f4ff209ead0d
Ajit Jadhav <mr.ajitjadhav@gmail.com>
2019-12-02 21:06:24 +0530

    Update setup_venv.sh

--- setup_venv.sh
+++ setup_venv.sh
@@ -2,3 +2,4 @@
 mkdir venv
 python3 -m venv ./venv && source ./venv/bin/activate
 python3 -m pip install flask flask_cors html5lib bs4 humanfriendly ultimate-sitemap-parser lxml
+echo "activate virtul environment by: 'source ./venv/bin/activate'"
