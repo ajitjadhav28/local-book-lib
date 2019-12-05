14846fa9f988bcbe153f41941f74e1eef94a3326
Ajit Jadhav <mr.ajitjadhav@gmail.com>
2019-12-05 18:38:45 +0530

    Added gunicorn to use app

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir venv
 python3 -m venv ./venv && source ./venv/bin/activate
-python3 -m pip install flask flask_cors html5lib bs4 humanfriendly ultimate-sitemap-parser lxml
+python3 -m pip install flask flask_cors html5lib bs4 humanfriendly ultimate-sitemap-parser lxml gunicorn
 echo "activate virtul environment by: 'source ./venv/bin/activate'"
